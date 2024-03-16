3e041588106af04d6e0fb8900b5dedf3812f6a8b
anagh555i <163624747+anagh555i@users.noreply.github.com>
2024-03-16 22:33:03 +0530

    Delete codeinit directory

--- codeinit/startup_page.js
+++
@@ -1,8 +0,0 @@
-let cnvrt=document.querySelector("#convert");
-cnvrt.addEventListener("click",funct);
-
-function funct(){
-    let head=document.querySelector(".heading");
-    if(head.innerHTML=="- . .-.. . --. .-. .- .--. ....") head.innerHTML="T E L E G R A P H";
-    else head.innerHTML="- . .-.. . --. .-. .- .--. ....";
-}
